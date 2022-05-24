345a8fc028ca8d5ecf0a340032e6ada7fa3f0c76
ibrahim-dev <ibrahim.devwork@gmail.com>
2022-05-24 17:38:00 +0100

    UserSeeder update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,13 +21,85 @@ class UserSeeder extends Seeder
         User::truncate();
         Schema::enableForeignKeyConstraints();
 
-        $user = new User();
-        $user->first_name = 'ibrahim';
-        $user->last_name = 'AOULAD ABDERAHMAN';
-        $user->username = 'Super admin';
-        $user->email = 'Admin@gmail.com';
-        $user->password = Hash::make('Admin123');
-        $user->id_role = 1;
+        $user               = new User();
+        $user->first_name   = 'ibrahim';
+        $user->last_name    = 'AOULAD ABDERAHMAN';
+        $user->username     = 'Super admin';
+        $user->email        = 'Admin@gmail.com';
+        $user->password     = Hash::make('Admin123');
+        $user->id_role      = 1;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'Admin01';
+        $user->email        = 'Admin01@gmail.com';
+        $user->password     = Hash::make('Admin01');
+        $user->id_role      = 2;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User01';
+        $user->email        = 'User01@gmail.com';
+        $user->password     = Hash::make('User01');
+        $user->id_role      = 3;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User02';
+        $user->email        = 'User02@gmail.com';
+        $user->password     = Hash::make('User02');
+        $user->id_role      = 3;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User03';
+        $user->email        = 'User03@gmail.com';
+        $user->password     = Hash::make('User03');
+        $user->id_role      = 3;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User04';
+        $user->email        = 'User04@gmail.com';
+        $user->password     = Hash::make('User04');
+        $user->id_role      = 3;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User05';
+        $user->email        = 'User05@gmail.com';
+        $user->password     = Hash::make('User05');
+        $user->id_role      = 3;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User06';
+        $user->email        = 'User06@gmail.com';
+        $user->password     = Hash::make('User06');
+        $user->id_role      = 3;
+        $user->save();
+
+        $user               = new User();
+        $user->first_name   = '';
+        $user->last_name    = '';
+        $user->username     = 'User07';
+        $user->email        = 'User07@gmail.com';
+        $user->password     = Hash::make('User07');
+        $user->id_role      = 3;
         $user->save();
     }
 }
